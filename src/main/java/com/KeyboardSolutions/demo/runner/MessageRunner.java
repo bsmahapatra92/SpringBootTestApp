d40affc5a4e7c37001ba9608e8b9570bffc44c88
bsmah <bsmah@LAPTOP-RK1S16SC>
2020-09-13 21:51:52 +0530

    wh-8520 First Time File Commit

---
+++ src/main/java/com/KeyboardSolutions/demo/runner/MessageRunner.java
@@ -0,0 +1,15 @@
+package com.KeyboardSolutions.demo.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageRunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Welcome To SpringBoot Application!!!");
+
+	}
+
+}
